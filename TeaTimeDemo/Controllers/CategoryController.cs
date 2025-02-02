bf89b73113bad0e78628eceb55207d1091d8377b
Jane <cat35150123@gmail.com>
2025-02-02 21:32:55 +0800

    加入專案檔案。

---
+++ TeaTimeDemo/Controllers/CategoryController.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TeaTimeDemo.Data;
+using TeaTimeDemo.Models;
+
+namespace TeaTimeDemo.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("DisplayOrder", "顯示順序不可以和名稱一樣");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+
+                TempData["success"] = "類別新增成功!";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "類別修改成功!";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                TempData["error"] = "類別修改失敗!";
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                TempData["error"] = "類別修改失敗!";
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                TempData["error"] = "類別刪除失敗!";
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                TempData["error"] = "類別刪除失敗!";
+                return NotFound();
+            }
+          
+            return View(categoryFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            Category? obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                TempData["error"] = "類別刪除失敗!";
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "類別刪除成功!";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ TeaTimeDemo/Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TeaTimeDemo.Models;
+
+namespace TeaTimeDemo.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Black Tea", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Green Tea", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Herbal Tea", DisplayOrder = 3 },
+                new Category { Id = 4, Name = "Oolong Tea", DisplayOrder = 4 },
+                new Category { Id = 5, Name = "White Tea", DisplayOrder = 5 },
+                new Category { Id = 6, Name = "Yellow Tea", DisplayOrder = 6 }
+            );
+        }
+    }
+
+
+}
